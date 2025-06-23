b93575c3be396b1f4ff566831c497881e990caf0
FGabriel-Lima <fgabriellimace@gmail.com>
2025-06-23 14:41:51 -0300

    feat: adicionando tela de login e cadastro de usuário

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
+import './App.css'; 
 
 function App() {
   return (
-    <>
-      <h1>Olá, mundo</h1>
-      <h1>Seja bem vindo(a) ao Feira Solidária!</h1>
-    </>
-  )
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/cadastro" element={<RegisterPage />} />
+          
+          {/* Rota para a página inicial */}
+          <Route path="/" element={
+            <div className="container text-center mt-5">
+              <h1>Bem-vindo ao Solidário</h1>
+              <p>Sua plataforma de doações.</p>
+            </div>
+          } />
+
+          {/* Redireciona qualquer rota não encontrada para a página inicial */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
